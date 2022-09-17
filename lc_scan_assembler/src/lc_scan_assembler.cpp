861dcca43a0eeb1e449d84ba40ab770cddc775ae
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-09-17 21:27:31 +0200

    move to c++ for pc assembly

---
+++ lc_scan_assembler/src/lc_scan_assembler.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/point_cloud2_iterator.hpp"
+#include "pcl_conversions/pcl_conversions.h"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world lc_scan_assembler package\n");
+  return 0;
+}
